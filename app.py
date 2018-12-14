f97209ac8f8911a888322c832fc05e3556847366
Ubuntu <wuhuikai@DeepCV.wzlonjquws4ujhmwhetvebl5de.bx.internal.cloudapp.net>
2018-12-14 13:38:14 +0000

    Enable CORS

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from api.uploader import Uploader
 from api.deep_guided_filter import ImageProcessing
 
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 
 
 app = Flask(__name__)
+CORS(app)
 deep_api = Api(app)
 deep_api.add_resource(Uploader, '/upload')
 deep_api.add_resource(ImageProcessing, '/<string:task>')
