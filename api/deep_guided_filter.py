0f47cdd6d17f7f48334d0e368365b7506307cde3
wuhuikai <huikaiwu@icloud.com>
2018-12-20 23:18:45 +0800

    LR ONLY

--- api/deep_guided_filter.py
+++ api/deep_guided_filter.py
@@ -11,8 +11,8 @@ from flask_restful import Resource
 def to_tensor(x):
     return transforms.ToTensor()(x).unsqueeze(0)
 
-def to_img(tensor):
-    return Image.fromarray(np.asarray(np.clip(np.squeeze(tensor.data.cpu().numpy()) * 255, 0, 255), dtype=np.uint8).transpose((1, 2, 0)))
+def to_array(tensor):
+    return np.squeeze(tensor.data.cpu().numpy()).transpose((1, 2, 0)).tolist()
 
 models = {task: torch.jit.load(os.path.join('models', task, 'hr_net_latest_jit.pth'))
           for task in ['auto_ps', 'l0_smooth', 'multi_scale_detail_manipulation', 'style_transfer', 'non_local_dehazing']}
@@ -23,20 +23,12 @@ class ImageProcessing(Resource):
             path = request.form['path']
             r, eps = int(request.form['r']), float(request.form['eps'])
 
-            im_hr = Image.open(path).convert('RGB')
-            im_lr = transforms.Resize(64, interpolation=Image.NEAREST)(im_hr)
-            im_hr = to_tensor(im_hr)
+            im_lr = Image.open(path).convert('RGB')
             im_lr = to_tensor(im_lr)
 
-            name = os.path.splitext(os.path.basename(path))[0]
-            save_name = '{}_{}_{}_{}.png'.format(name, task, r, eps)
-            save_path = os.path.join('static', 'images', save_name)
+            A, b = models[task](im_lr, r, eps)
+            A, b = to_array(A), to_array(b)
 
-            image = to_img(models[task](im_lr, im_hr, r, eps))
-            if task == 'style_transfer':
-                image = image.convert('L')
-            image.save(save_path)
-
-            return {'data': save_path, 'status': 'success'}
+            return {'A': A, 'b': b, 'status': 'success'}
 
         return {'status': 'error'}
--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_restful import Api
 app = Flask(__name__)
 deep_api = Api(app)
 deep_api.add_resource(Uploader, '/upload')
-deep_api.add_resource(ImageProcessing, '/<string:task>')
+deep_api.add_resource(ImageProcessing, '/deep_guided_filter/<string:task>')
 
 if __name__ == '__main__':
     app.run(debug=True)
