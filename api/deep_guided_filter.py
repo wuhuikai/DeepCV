334b6c8d60179429983164bfeb6f31e77be49ae3
wuhuikai <huikaiwu@icloud.com>
2019-01-14 20:36:55 +0800

    get [fix]

--- api/deep_guided_filter.py
+++ api/deep_guided_filter.py
@@ -5,8 +5,7 @@ import numpy as np
 from PIL import Image
 from torchvision import transforms
 
-from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 
 def to_tensor(x):
@@ -22,10 +21,15 @@ models = {task: torch.jit.load(os.path.join('models', task, 'hr_net_latest_jit.p
 
 
 class ImageProcessing(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('path', type=str)
+    parser.add_argument('r', type=int)
+    parser.add_argument('eps', type=float)
+
     def get(self, task):
         if task in models:
-            path = request.form['path']
-            r, eps = int(request.form['r']), float(request.form['eps'])
+            args = ImageProcessing.parser.parse_args()
+            path, r, eps = args['path'], args['r'], args['eps']
 
             im_lr = Image.open(path).convert('RGB')
             im_lr = to_tensor(im_lr)
