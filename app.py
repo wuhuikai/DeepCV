5a1fc78fdbe0ea3d094fd3f5e7527e873bcf6178
wuhuikai <huikaiwu@icloud.com>
2019-02-13 08:35:15 +0000

    Compress

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ from api.blending import Blending
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
+from flask_compress import Compress
 
 from flask import send_from_directory
 
-
 app = Flask(__name__)
 CORS(app)
+Compress().init_app(app)
 deep_api = Api(app)
 deep_api.add_resource(Uploader, '/upload')
 deep_api.add_resource(ImageProcessing, '/deep_guided_filter/<string:task>')
@@ -26,4 +27,4 @@ def get_image(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='8080')
+    app.run(debug=True, host='0.0.0.0', port='8888', threaded=True)
